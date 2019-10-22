66eff8519db68cb25dcbef151c112fbc3607b088
Abdallah Yashir <ayashir@ext.absystech.fr>
2019-10-22 13:53:36 +0400

    Add more in home vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,24 +9,53 @@
       :value="today"
       :weekdays="weekdays"
       :events="events"
-      ></v-calendar>
-    </v-sheet>
-  </v-container>
+      >
+      <!-- the events at the top (all-day) -->
+      <template v-slot:day-header="{ date }">
+        <template v-for="event in eventsMap[date]">
+          <!-- all day events don't have time -->
+          <div
+          v-if="!event.time"
+          :key="event.title"
+          class="my-event"
+          @click="open(event)"
+          v-html="event.title"
+          ></div>
+        </template>
+      </template>
+      <!-- the events at the bottom (timed) -->
+      <template v-slot:day-body="{ date, timeToY, minutesToPixels }">
+        <template v-for="event in eventsMap[date]">
+          <!-- timed events -->
+          <div
+          v-if="event.time"
+          :key="event.title"
+          :style="{ top: timeToY(event.time) + 'px', height: minutesToPixels(event.duration) + 'px' }"
+          class="my-event with-time"
+          @click="open(event)"
+          v-html="event.title"
+          ></div>
+        </template>
+      </template>
+    </v-calendar>
+  </v-sheet>
+</v-container>
 </template>
 
 <script>
   import { DateTime } from 'luxon'
+  const now = DateTime.local()
 
   export default {
     data: () => ({
-      today: DateTime.local().toLocaleString(),
+      today: now.toLocaleString(),
       weekdays: [1, 2, 3, 4, 5],
       events: [
-      {
-        name: 'Meeting',
-        start: '2019-01-07',
-        end: '2019-01-07'
-      },
+        {
+          name: 'Weekly Meeting',
+          start: '2019-10-22 09:00',
+          end: '2019-10-22 10:00',
+        },
       ],
     }),
     mounted () {
